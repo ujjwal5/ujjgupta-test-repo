214a09c8029e58c631db9fb886785aaaecc5bb22
Ujjwal Gupta <ujjwal5@users.noreply.github.com>
2024-04-02 11:56:50 +0530

    Initialize dev

---
+++ blocks/form/constant.js
@@ -0,0 +1,21 @@
+export const fileAttachmentText = 'Attach';
+export const dragDropText = 'Drag and Drop To Upload';
+
+export const DEFAULT_THANK_YOU_MESSAGE = 'Thank you for your submission.';
+
+export const defaultErrorMessages = {
+  accept: 'The specified file type not supported.',
+  maxFileSize: 'File too large. Reduce size and try again.',
+  maxItems: 'Specify a number of items equal to or less than $0.',
+  minItems: 'Specify a number of items equal to or greater than $0.',
+};
+
+let submitBaseUrl = '';
+
+export function setSubmitBaseUrl(url) {
+  submitBaseUrl = url;
+}
+
+export function getSubmitBaseUrl() {
+  return submitBaseUrl;
+}
